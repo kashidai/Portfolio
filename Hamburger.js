159b74de9d06bcec20a364d54198ee342bbe559e
Dai <d.k.95.82.8192@icloud.com>
2019-11-27 22:35:57 -0800

    make scroll

--- Hamburger.js
+++ Hamburger.js
@@ -7,5 +7,15 @@ $(function () {
         } else {
             $('.NavMenu').removeClass('active'); //クラスを外す
         }
+
+        $('#nav a').on('click', function() {
+            $('#nav').toggleClass('active');
+            $(".Toggle").toggleClass('active');
+        });
+
+
     });
 });
+
+
+
